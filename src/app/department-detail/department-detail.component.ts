a5344e40dff20aa2d980a8210929431367b9b67b
arpit.nagpal <arpit.nagpal@jungleworks.com>
2018-08-04 17:42:35 +0530

    Next and previous buttons in department-details

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router"
+import { ActivatedRoute, Router, ParamMap } from "@angular/router"
 @Component({
   selector: 'app-department-detail',
   template: `
     <p>
-      Department Selected is: {{id}}
+      Department Selected is: {{department_id}}
     </p>
+    <a (click)="goPrevious()">Previous</a>
+    <a (click)="goNext()">Next</a>
   `,
   styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
 
-  public id;
+  public department_id;
 
-  constructor(private router: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    let id = parseInt(this.router.snapshot.paramMap.get("id"));
-    this.id = id;
+    // let id = parseInt(this.route.snapshot.paramMap.get("id"));
+    // this.department_id = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get("id"))
+      this.department_id = id;
+    })
+  }
+
+  goPrevious(){
+    let previous_id = this.department_id - 1
+    this.router.navigate(["/departments", previous_id])
+  }
+
+  goNext(){
+    let next_id = this.department_id + 1
+    this.router.navigate(["/departments", next_id])
   }
 
 
